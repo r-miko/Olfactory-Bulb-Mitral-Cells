042a78210e1ebe8e961c9f088938e61f1f24a5be
r-miko <rebeccamiko@outlook.com>
2017-11-10 15:49:48 +0000

    Cells, Network and Single-cells folders added.

---
+++ Temporal-Filtering/Single-Cells/MCs/Channels/x86_64/cadecay2.mod
@@ -0,0 +1,1 @@
+../cadecay2.mod
---
+++ Temporal-Filtering/Single-Cells/PGCs/Channels/x86_64/mod_func.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "hocdec.h"
+extern int nrnmpi_myid;
+extern int nrn_nobanner_;
+
+extern void _CaPN_reg(void);
+extern void _CaT_reg(void);
+extern void _Caint_reg(void);
+extern void _Can_reg(void);
+extern void _KCa_reg(void);
+extern void _KDRmt_reg(void);
+extern void _KS_reg(void);
+extern void _LCa_reg(void);
+extern void _NaP_reg(void);
+extern void _Naxn_reg(void);
+extern void _OdorInput_reg(void);
+extern void _cadecay_reg(void);
+extern void _cadecay2_reg(void);
+extern void _hpg_reg(void);
+extern void _kAmt_reg(void);
+extern void _kM_reg(void);
+extern void _kfasttab_reg(void);
+extern void _kslowtab_reg(void);
+extern void _nafast_reg(void);
+
+void modl_reg(){
+  if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+    fprintf(stderr, "Additional mechanisms from files\n");
+
+    fprintf(stderr," CaPN.mod");
+    fprintf(stderr," CaT.mod");
+    fprintf(stderr," Caint.mod");
+    fprintf(stderr," Can.mod");
+    fprintf(stderr," KCa.mod");
+    fprintf(stderr," KDRmt.mod");
+    fprintf(stderr," KS.mod");
+    fprintf(stderr," LCa.mod");
+    fprintf(stderr," NaP.mod");
+    fprintf(stderr," Naxn.mod");
+    fprintf(stderr," OdorInput.mod");
+    fprintf(stderr," cadecay.mod");
+    fprintf(stderr," cadecay2.mod");
+    fprintf(stderr," hpg.mod");
+    fprintf(stderr," kAmt.mod");
+    fprintf(stderr," kM.mod");
+    fprintf(stderr," kfasttab.mod");
+    fprintf(stderr," kslowtab.mod");
+    fprintf(stderr," nafast.mod");
+    fprintf(stderr, "\n");
+  }
+  _CaPN_reg();
+  _CaT_reg();
+  _Caint_reg();
+  _Can_reg();
+  _KCa_reg();
+  _KDRmt_reg();
+  _KS_reg();
+  _LCa_reg();
+  _NaP_reg();
+  _Naxn_reg();
+  _OdorInput_reg();
+  _cadecay_reg();
+  _cadecay2_reg();
+  _hpg_reg();
+  _kAmt_reg();
+  _kM_reg();
+  _kfasttab_reg();
+  _kslowtab_reg();
+  _nafast_reg();
+}
