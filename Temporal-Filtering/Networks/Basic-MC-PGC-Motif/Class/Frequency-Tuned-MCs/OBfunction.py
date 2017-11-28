018df74c90dd18e81e9a45fa198292e3dbc6fbdc
r-miko <rebeccamiko@outlook.com>
2017-11-28 13:44:09 +0000

    tuneMC.py file can now run all parameters.

--- Temporal-Filtering/Networks/Basic-MC-PGC-Motif/Class/Frequency-Tuned-MCs/OBfunction.py
+++ Temporal-Filtering/Networks/Basic-MC-PGC-Motif/Class/Frequency-Tuned-MCs/OBfunction.py
@@ -49,21 +49,13 @@ def runOB(frequency,ExFactor,InhFactor,PGFactor,record_vars,syn_vars,directory,f
 	# Input current
 	input_current = np.ones((tstop,))*[np.cos((t/1000.0)*(2*np.pi*frequency)) for t in time]*c2 + strength
 	input_current[0:500] = 0.0
-		
-
+	
 	pgc_input_current = (np.ones((tstop,))*[np.cos((t/1000.0)*(2*np.pi*frequency)) for t in time]*c2 + strength)*PGFactor
 	pgc_input_current[0:500] = 0.0
 
 	# Record membrane potential and time
 	variables = model.record_membranept(record_vars)
 	variables = model.record_time(variables)
-
-	# Record synaptic currents
-	#if mc_pgc_excitation:
-	#	syn_vars.append("mc_pgc_ampa")
-	#	syn_vars.append("mc_pgc_nmda")
-	#if pgc_mc_inhibition: 
-	#	syn_vars.append("pgc_mc_gaba")
 		
 	synvariables = model.record_syn_currents(syn_vars)
 
@@ -77,31 +69,11 @@ def runOB(frequency,ExFactor,InhFactor,PGFactor,record_vars,syn_vars,directory,f
 	if len(MC_spiketimes_list) > 0:
 		MC_first_spike_latency = model.MC_first_spike_latency(MC_spiketimes_list, a = 500)
 		MC_interspike_freq = model.MC_spike_frequencies(MC_spiketimes_list)
-	'''
-	vec2 = variables[2]
-	p = np.array(vec2)
-	PGC_spiketimes_list = model.PGC_spike_times(p, threshold = 0)
-	if len(PGC_spiketimes_list) > 0:
-		PGC_first_spike_latency = model.PGC_first_spike_latency(PGC_spiketimes_list, a = 500)
-		PGC_interspike_freq = model.PGC_spike_frequencies(PGC_spiketimes_list)
-	'''
-	# PLOTTING AND SAVING		
 
-		
+	# PLOTTING AND SAVING			
 	# Plotting membrane potential and input current
 	t_vec = variables[-1]
-	'''
-	for i,x in enumerate(record_vars):
-		variable = variables[i]
-		if x is "mc_soma":
-			model.plotMemPotential_MCSoma(t_vec, variable, "Membrane Potential at MC Soma", directory + filename + "MC_Soma.png")
-		if x is "mc_tuft":
-			model.plotMemPotential_tuft(t_vec, variable, "Membrane Potential at MC tuft", directory + filename +"MC_Tuft.png")
-		if x is "pgc_gemmbody":
-			model.plotMemPotential_PGCgemmbody(t_vec, variable, "Membrane Potential at PGC gemmbody", directory + filename +"PGC_gemmbody.png")
-	'''
-	#model.plotInputCurrent(time, input_current, directory+"Input_Current.png")
-		
+	
 	# Plotting synaptic currents
 	if len(syn_vars) is 1:
 		model.plotSynapticCurrent_mcGABA(t_vec, synvariables[0], "Synaptic Current at mcGABA", directory +filename + "SC_mcGABA.png")
@@ -114,35 +86,10 @@ def runOB(frequency,ExFactor,InhFactor,PGFactor,record_vars,syn_vars,directory,f
 		model.plotSynapticCurrent_pgNMDA(t_vec, synvariables[2], "Synaptic Current at pgNMDA", directory + filename +"SC_pgNMDA.png")
 
 	# Save
-	#np.save(directory+ filename +"Input_current",input_current)
 	for i,x in enumerate(record_vars):
 		variable = variables[i]
 		np.save(directory +  filename +x, variable)
-	#np.save(directory + filename + "Time", variables[-1]) 
 	np.save(directory + filename + "MC_Spiketimes", MC_spiketimes_list)
-	#if len(MC_spiketimes_list) > 0:
-	#	np.save(directory + filename + "MC_Interspike_frequencies", MC_interspike_freq)
-	#	np.save(directory +  filename +"MC_First_spike_latency", MC_first_spike_latency)
-	#if len(MC_spiketimes_list) == 0:
-	#	np.save(directory + filename + "MC_Interspike_frequencies", -1)
-	#	np.save(directory +  filename +"MC_First_spike_latency", -1)
-	'''
-	np.save(directory + filename + "PGC_Spiketimes", PGC_spiketimes_list)
-	if len(PGC_spiketimes_list) > 0:
-		np.save(directory +  filename +"PGC_Interspike_frequencies", PGC_interspike_freq)
-		np.save(directory +  filename +"PGC_First_spike_latency", PGC_first_spike_latency)
-	if len(PGC_spiketimes_list) == 0:
-		np.save(directory +  filename +"PGC_Interspike_frequencies", -1)
-		np.save(directory + filename + "PGC_First_spike_latency", -1)
-	if len(syn_vars) is 1:
-		np.save(directory +  filename +"pgc_mc_gaba", synvariables[0])
-	if len(syn_vars) is 2:
-		np.save(directory + filename + "mc_pgc_ampa", synvariables[0])
-		np.save(directory +  filename +"mc_pgc_nmda", synvariables[1])
-	if len(syn_vars) is 3:
-		np.save(directory +  filename +"mc_pgc_ampa", synvariables[0])
-		np.save(directory + filename + "mc_pgc_nmda", synvariables[1])
-		np.save(directory +  filename +"pgc_mc_gaba", synvariables[2])
-	'''
 	plt.close("all")
+
 
--- Temporal-Filtering/Networks/Basic-MC-PGC-Motif/Class/Frequency-Tuned-MCs/tuneMC.py
+++ Temporal-Filtering/Networks/Basic-MC-PGC-Motif/Class/Frequency-Tuned-MCs/tuneMC.py
@@ -1,47 +1,83 @@
-# File which creates and runs a model from OBTestClass.py
+# File used to tune parameters for feed-forward inhibition
 
 from neuron import h,gui
 import tabchannels
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 from OBfunction import runOB
 
-
-
-
+# Function that runs the OB with the given parameters
 def runBatch(ExFactor,InhFactor,PGFactor,frequencies):
+	# Parameters
 	record_vars = ['mc_soma']
 	syn_vars    = []
-	directory   = ''
-	fname    = 'test'
+	directory   = 'npy_files/'
 
-	for  frequency in frequencies:
+	# For each frequency, run function called from OBfunction
+	for frequency in frequencies:
 		print frequency
-		filename = fname + '_' + str(frequency) +'_'
+		filename = 'f' + str(frequency) + '_e' + str(ExFactor) + '_i' + str(InhFactor) + '_p' + str(PGFactor) + '_'
 		runOB(frequency,ExFactor,InhFactor,PGFactor,record_vars,syn_vars,directory,filename)
 
+# Function that analyses the batch
 def analyseBatch(frequencies):
+	# Parameters
 	directory   = ''
-	fname    = 'test'
 	tuning_curve = np.zeros((len(frequencies),))
-	for  i,frequency in enumerate(frequencies):
-		filename = fname + '_' + str(frequency) +'_'+'MC_Spiketimes.npy'
+	
+	# For each frequency create tuning curve from firing rate
+	for  j,frequency in enumerate(frequencies):
+		filename = 'f' + str(frequency) + '_e' + str(e) + '_i' + str(i) + '_p' + str(p) +'_' + 'MC_Spiketimes.npy'
 		spikes= np.load(filename)
-		tuning_curve[i] = len(spikes)/3.0
+		tuning_curve[j] = len(spikes)/3.0
 	return tuning_curve
 
+# Function that plots the tuning curve
 def plotTuningCurve(frequencies,tuning_curve):
-	fig = plt.figure(figsize=(15,25))
+	# Figure
+	fig = plt.figure(figsize=(25,15))
 	ax = fig.add_subplot(111)
-	ax.plot(frequencies,tuning_curve,'k*',markersize=5.0)
-	plt.show()
 
+	# Plot frequency against the tuning_curve
+	ax.plot(frequencies,tuning_curve,'k*',markersize=5.0)
+
+	# Labels and Legend
+	plt.title("MC Tuning", fontsize = 34)
+	ax.title.set_position([0.5, 1.03])
+	plt.ylabel("Firing Rate (Hz)", fontsize = 30)
+	plt.xlabel("Frequency (Hz)", fontsize = 30)
+	plt.xticks(fontsize = 23)
+	plt.yticks(fontsize = 23)
+
+	#plt.show()
+	return fig
+
+# Function that saves the plot
+def saveTuningCurve(figure):
+	print "Saving Plots"
+	directory = "Tuning_Results/"
+	filename = 'e' + str(e) + '_i' + str(i) + '_p' + str(p)
+	
+	# Save
+	plt.savefig(directory + filename + '.png', dpi = 300)
+
+# Main
 if __name__ == "__main__":
-	frequencies   = np.arange(1.0,40.0,1.0)
-	ExFactor  = 3.33
-	InhFactor = 4.0
-	PGFactor  = 0.6
-	runBatch(ExFactor,InhFactor,PGFactor,frequencies)
-	tuning_curve = analyseBatch(frequencies)
-	plotTuningCurve(frequencies, tuning_curve)
+	#Parameters
+	frequencies   = np.arange(1.0, 40.0, 1.0)
+	ExFactor  = [3.33, 6.66, 10.0]
+	InhFactor = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+	PGFactor  = [0.2, 0.4, 0.6, 0.8, 1.0]
+
+	#For each combination of the above parameters, run the following functions
+	for e in ExFactor:
+		for i in InhFactor:
+			for p in PGFactor:
+				runBatch(e,i,p,frequencies)
+				tuning_curve = analyseBatch(frequencies)
+				fig = plotTuningCurve(frequencies, tuning_curve)
+				saveTuningCurve(fig)
+
+	
