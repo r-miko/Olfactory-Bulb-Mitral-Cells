900ddb2fc954c20203e0e48152795ba4c1a94886
r-miko <rebeccamiko@outlook.com>
2017-12-14 12:58:39 +0000

    Contour plot

--- Temporal-Filtering/Networks/Basic-MC-PGC-Motif/Class/Frequency-Tuned-MCs/tuneMC2.py
+++ Temporal-Filtering/Networks/Basic-MC-PGC-Motif/Class/Frequency-Tuned-MCs/tuneMC2.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
 from OBfunction import runOB
 
 # Function that runs the OB with the given parameters
@@ -24,27 +29,27 @@ def runBatch(ExFactor,InhFactor,PGFactor,frequencies):
 # Function that analyses the batch for firing rates and latency
 def analyseBatch(frequencies):
 	# Parameters
-	directory   = 'npy_files/'
-	TC = []
-	tuning_curveFR = np.zeros((len(frequencies),))
-	tuning_curveL = np.zeros((len(frequencies),))
+	directory	= 'npy_files/'
+	TC		= []
+	tuning_curveFR	= np.zeros((len(frequencies),))
+	tuning_curveL	= np.zeros((len(frequencies),))
 	
 	# For each frequency create tuning curve from firing rate
 	for  j,frequency in enumerate(frequencies):
 		filenameFR = 'f' + str(frequency) + '_e' + str(e) + '_i' + str(i) + '_p' + str(p) +'_' + 'MC_Spiketimes.npy'
-		spikes= np.load(directory+filenameFR)
+		spikes = np.load(directory+filenameFR)
 		tuning_curveFR[j] = len(spikes)/3.0
 	TC.append(tuning_curveFR)
 	# For each frequency create tuning curve from latency
 	for  j,frequency in enumerate(frequencies):
 		filenameL = 'f' + str(frequency) + '_e' + str(e) + '_i' + str(i) + '_p' + str(p) +'_' + 'MC_First_spike_latency.npy'
 		latency = np.load(directory+filenameL)
-		#tuning_curveL[j] = len(latency)/3.0
+		tuning_curveL[j] = latency
 	TC.append(tuning_curveL)
 	return TC
 
 # Function that plots the tuning curve
-def plotTuningCurve(frequencies,tuning_curve, ylabel):
+def plotTuningCurve(frequencies,tuning_curve,ylabel):
 	# Figure
 	fig = plt.figure(figsize=(25,15))
 	ax = fig.add_subplot(111)
@@ -59,11 +64,11 @@ def plotTuningCurve(frequencies,tuning_curve, ylabel):
 	plt.xlabel("Frequency (Hz)", fontsize = 30)
 	plt.xticks(fontsize = 23)
 	plt.yticks(fontsize = 23)
-
+	
 	#plt.show()
 	return fig
 
-# Function that saves the plot
+# Function that saves the tuning plot
 def saveTuningCurve(figure, feature):
 	print "Saving Plots..."
 	directory = "Tuning_Results2/"
@@ -71,6 +76,66 @@ def saveTuningCurve(figure, feature):
 	
 	# Save
 	plt.savefig(directory + filename + '.png')
+	plt.close()
+
+	
+def extractPeak(tuning_curve):
+	peak = np.argmax(tuning_curve)
+
+	return peak
+
+def plotPeaks(peaks, X, Y):
+	# Figure
+	fig = plt.figure(figsize = (25,15))
+	ax = fig.gca(projection = '3d')
+	
+	# Plot
+	X,Y = np.meshgrid(X, Y)
+	surf = ax.plot_surface(X, Y, peaks, cmap = cm.coolwarm, linewidth = 0, antialiased = False)
+
+	# Labels and Legend
+	plt.title("Peak Frequency of the Tuning Curve", fontsize = 34)
+	ax.title.set_position([0.5, 1.03])
+	ax.set_xlabel("Ex Factor", fontsize = 30)
+	ax.set_ylabel("Inh Factor", fontsize = 30)
+	ax.set_zlabel("Peak Frequency (Hz)", fontsize = 30)
+	plt.xticks(fontsize = 23)
+	plt.yticks(fontsize = 23)
+
+	#Add a color bar which maps values to colors.
+	#fig.colorbar(surf, shrink=0.5, aspect=5)
+	#plt.colorbar(surf)
+
+	#plt.show()
+	return fig
+
+def plotPeaks2(peaks, X, Y):
+	# Figure 
+	plt.figure(figsize = (25,15))
+
+	# Plot
+	fig = plt.imshow(peaks, extent=(X.min(), X.max(), Y.max(), Y.min()), interpolation = 'bilinear',cmap = cm.coolwarm)
+
+	# Labels and Legend
+	plt.title("Peak Frequency of the Tuning Curve", fontsize = 34, y = 1.08)
+	plt.xlabel("Excitation Factor", fontsize = 20)
+	plt.ylabel("Inhibitory Factor", fontsize = 20)
+
+	#Add a color bar which maps values to colors.
+	plt.colorbar(fig)
+
+	#plt.show()
+	return fig
+
+# Function that saves the contour plot
+def saveContourPlot(figure):
+	print "Saving Plots..."
+	directory = "Contour_Plots/"
+	filename = 'PGFactor' + str(p)
+	
+	# Save
+	plt.savefig(directory + filename + '.png')
+	plt.close()
 
 # Main
 if __name__ == "__main__":
@@ -80,15 +145,30 @@ if __name__ == "__main__":
 	InhFactor = [1.0, 2.0, 3.0, 4.0, 5.0]
 	PGFactor  = [0.2, 0.3, 0.4, 0.5, 0.6]
 
-	#For each combination of the above parameters, run the following functions
-	for e in ExFactor:
-		for i in InhFactor:
-			for p in PGFactor:
-				runBatch(e,i,p,frequencies)
-				TC = analyseBatch(frequencies)
-				figFR = plotTuningCurve(frequencies, TC[1], "Firing Rate (Hz)")
-				figL = plotTuningCurve(frequencies, TC[0], "Latency (ms)")
-				saveTuningCurve(figFR, "FR")
-				saveTuningCurve(figL, "L")
 
-	
+	FRpeaks = np.zeros((5,5,5))
+
+	#For each combination of the above parameters, run the following functions
+	for j,e in enumerate(ExFactor):
+		for k,i in enumerate(InhFactor):
+			for l,p in enumerate(PGFactor):
+				#runBatch(e,i,p,frequencies)
+				TC = analyseBatch(frequencies)
+				# Plot and save firing rates
+				#figFR = plotTuningCurve(frequencies, TC[0], "Firing Rate (Hz)")
+				#saveTuningCurve(figFR, "FR")
+				# Plot and save latency
+				#figL = plotTuningCurve(frequencies, TC[1], "Latency (ms)")
+				#saveTuningCurve(figL, "L")
+				FRpeaks[j,k,l] = extractPeak(TC[0])
+
+	# Plot peaks 
+	for m in range(len(PGFactor)):
+		fig = plotPeaks2(FRpeaks[:,:,m], np.array(ExFactor), np.array(InhFactor))
+	# Save peak plots
+	for l,p in enumerate(PGFactor):
+		saveContourPlot(fig)
+		
+
+
+
